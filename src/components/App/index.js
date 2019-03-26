ce22468ddb4d6f7142d3a88ae826a8383b5cf796
cm <cm@cms-MacBook-Pro.local>
2019-03-26 11:16:00 +0800

    remove context

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,21 +1,18 @@
 import React from 'react'
 
 import { ThemedButton, Text, Text2 } from '../Text'
-import { StateProvider, cmProvider } from '../../store'
 
 const App = () => {  
   console.log('App')
   return (
-    <cmProvider>
-      <div>
-        <header>i am header</header>
-        <div>123</div>
-        <ThemedButton />
-        <Text />
-        <Text2 />
-        <footer>i am footer</footer>
-      </div>
-    </cmProvider>
+    <div>
+      <header>i am header</header>
+      <div>123</div>
+      <ThemedButton />
+      <Text />
+      <Text2 />
+      <footer>i am footer</footer>
+    </div>
   );
 }
 
--- src/components/Text/index.js
+++ src/components/Text/index.js
@@ -1,6 +1,6 @@
 import React, {useContext} from 'react'
 
-import { useStateValue, StateContext } from '../../store';
+// import { useStateValue, StateContext } from '../../store';
 
 export const Text2 = () => {
   console.log('Text2')
@@ -11,24 +11,26 @@ export const Text2 = () => {
 
 
 export const Text = () => {
-  const [s, dispatch] = useStateValue()
-  console.log(s,'Text')
-  const theme = s.theme
+  // const [s, dispatch] = useStateValue()
+  // console.log(s,'Text')
+  // const theme = s.theme
   return (
-    <div style={{ color: theme.primary }}>I am text</div>
+    <div style={{ 
+      // color: theme.primary
+     }}>I am text</div>
   )
 }
 
 export const ThemedButton = () => {
-  const [{ theme }, dispatch] = useStateValue()
+  // const [{ theme }, dispatch] = useStateValue()
   console.log('Button')
   return (
     <button
-      style={{ backgroundColor: theme.primary }}
-      onClick={() => dispatch({
-        type: 'changeTheme',
-        newTheme: 'blue'
-      })}
+      // style={{ backgroundColor: theme.primary }}
+      // onClick={() => dispatch({
+      //   type: 'changeTheme',
+      //   newTheme: 'blue'
+      // })}
     >
       Make me blue!
     </button>
