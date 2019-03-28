fe4b7d6737c734c1736576542cc839205cc45aa3
cm <cm@cms-MacBook-Pro.local>
2019-03-28 17:27:55 +0800

    fixed formatter

--- src/lib/util/format.js
+++ src/lib/util/format.js
@@ -1,5 +1,9 @@
+const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+})
 function toPrice(number, decimal = 2) {
-  return `$${number.toFixed(decimal).replace(/\d(?=(\d{3})+\.)/g, '$&,')}`
+  return formatter.format(number).slice(0, decimal === 0 ? -3 : decimal === 1 ? -1 : undefined)
 }
 
 export {toPrice}
