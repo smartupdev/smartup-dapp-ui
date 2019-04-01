32f398212c72e31d96b765b4af03b2a2cf0f4450
CM Ngan <plusonecm@gmail.com>
2019-04-01 15:18:25 +0800

    Add bookmarked

---
+++ src/components/Icon/Bookmarked.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { SVG } from './index'
+export default ({ color, ...rest }) => 
+  <SVG {...rest} color={color} version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 56.9 56.9" xmlSpace="preserve">
+    <path d="M52,6.3c0-3.3-2.7-6.1-6-6.1v0H9.1C5.7,0.1,3,2.9,3,6.2V57l19-12.7L41,57V25.1h11V6.3z M50,23.1h-9v-16
+      c0-0.3,0-0.7,0.1-1c0.4-1.9,1.9-3.4,3.7-3.9c0,0,0.1,0,0.1,0c0.2-0.1,0.5-0.1,0.8-0.1c0.1,0,0.2,0,0.2,0C48.2,2.2,50,4,50,6.3
+      L50,23.1z"/>
+  </SVG>
