60e20fbfd6cb040e1862a6eb1952d120c5a6389a
CM Ngan <plusonecm@gmail.com>
2019-04-09 16:01:23 +0800

    remove unused code

--- src/store.js
+++ src/store.js
@@ -2,26 +2,13 @@ import { applyMiddleware, createStore } from 'redux';
 import reducer from './reducer';
 import { createLogger } from 'redux-logger'
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
-import { promiseMiddleware, localStorageMiddleware } from './middleware';
 import thunk from 'redux-thunk';
 
-
-import { routerMiddleware } from 'react-router-redux'
-import createHistory from 'history/createBrowserHistory';
-
-export const history = createHistory();
-
-// Build the middleware for intercepting and dispatching navigation actions
-const myRouterMiddleware = routerMiddleware(history);
-
 const getMiddleware = () => {
-    if (process.env.NODE_ENV === 'production') {
-        return applyMiddleware(myRouterMiddleware, promiseMiddleware, localStorageMiddleware, thunk);
-    } else {
-        // Enable additional logging in non-production environments.
-        return applyMiddleware(myRouterMiddleware, promiseMiddleware, localStorageMiddleware, thunk, createLogger())
-    }
+    return process.env.NODE_ENV === 'production' ? 
+        applyMiddleware(thunk)
+    :
+        composeWithDevTools(applyMiddleware(thunk, createLogger()))
 };
 
-export const store = createStore(
-    reducer, composeWithDevTools(getMiddleware()));
+export const store = createStore(reducer, getMiddleware());
