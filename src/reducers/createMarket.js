d2512a4bcf699034f18f42e514377c3cdca552bb
CM Ngan <plusonecm@gmail.com>
2019-04-12 11:17:58 +0800

    Add create market value and onChange and error handling

--- src/reducers/createMarket.js
+++ src/reducers/createMarket.js
@@ -1,14 +1,46 @@
-import { SET_ACTIVE_INDEX } from '../actions/actionTypes';
+import { SET_ACTIVE_INDEX, MARKET_NAME_CHANGE, MARKET_DESC_CHANGE } from '../actions/actionTypes';
 
 export const initialState = {
   activeIndex: 0,
+  name: '',
+  desc: '',
+  error: {
+    name: '',
+    desc: ''
+  }
 }
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case MARKET_NAME_CHANGE: {
+      const error = action.payload.length < 3 || action.payload.length > 20 
+      return {
+        ...state,
+        name: action.payload,
+        error: {
+          ...state.error,
+          name: error
+        }
+      }
+    }
+    case MARKET_DESC_CHANGE: {
+      const error = action.payload.length < 1 || action.payload.length > 150 
+      return {
+        ...state,
+        desc: action.payload,
+        error: {
+          ...state.error,
+          desc: error
+        }
+      }
+    }
+
     case SET_ACTIVE_INDEX: {
-      const tempIndex = state.activeIndex + action.changeNumber;
-      return Object.assign({}, state, { activeIndex: tempIndex });
+      const block = state.error.name || state.error.desc
+      return {
+        ...state,
+        activeIndex: block ? state.activeIndex : action.payload
+      }
     }
     default:
       return state;
