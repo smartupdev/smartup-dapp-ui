bf745d8257dd2e65fc640fe57aea996db4311a29
CM Ngan <plusonecm@gmail.com>
2019-04-18 11:22:44 +0800

    fix icon disabled issue

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -15,7 +15,7 @@ export const Path = styled.path`
   ${p => p.passive && css`fill: ${p.theme.colorSecondary};`}
 `
 
-export const SVG = ({onClick, disabled, ...rest}) => <StyledSvg onClick={disabled ? undefined : onClick} disabled {...rest} />
+export const SVG = ({onClick, disabled, ...rest}) => <StyledSvg onClick={disabled ? undefined : onClick} disabled={disabled} {...rest} />
 const StyledSvg = styled.svg`
   display: block;
   width: ${p => 
