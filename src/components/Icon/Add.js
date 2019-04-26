72d318ef911538d6f9494c6e319c73f1f854c5d6
hkykit <hkykit@gmail.com>
2019-04-26 12:04:23 +0800

    Update icon.js

--- src/components/Icon/Add.js
+++ src/components/Icon/Add.js
@@ -1,3 +1,6 @@
+import React from 'react'
+import { SVG } from './index'
+
 <SVG {...rest}  color={color}  version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 	 viewBox="0 0 100 100" xmlSpace="preserve">
 <g>
