ca1b4e25b3fd7e71f8913881414e1e7579ac73b1
CM Ngan <plusonecm@gmail.com>
2019-04-28 14:14:58 +0800

    Add saved tab

--- src/containers/Dispute/index.js
+++ src/containers/Dispute/index.js
@@ -1,3 +1,5 @@
 import React from 'react'
+import Text from '../../components/Text'
+
 export default () => 
-  <div>Dispute</div>
+  <Text center VM note>This page is currently under development. Please check back again later.</Text>
