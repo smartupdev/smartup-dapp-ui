cf7c04e8827e1c381bc2ca4a07ead33ad31b4a04
unknown <karlis.chan@smartup.global>
2019-04-30 18:56:05 +0800

    add(icon):CreateMartketButton, FindMarketButton, ReadAll, UnreadMsg

---
+++ src/components/Icon/UnreadMsg.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { SVG } from './index'
+export default ({ color, ...rest }) =>
+
+<SVG version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" 
+
+     viewBox="0 0 100 100" xmlSpace="preserve"> 
+
+{/* <style type="text/css"> 
+
+    .st0{fill:none;stroke:#000000;stroke-width:6.25;stroke-linecap:round;stroke-miterlimit:10;} 
+
+</style>  */}
+
+<g> 
+
+    <g> 
+
+        <path d="M50,0C22.4,0,0,22.4,0,50s22.4,50,50,50s50-22.4,50-50S77.6,0,50,0z M50,93.4C26,93.4,6.6,74,6.6,50C6.6,26,26,6.6,50,6.6 
+
+            S93.4,26,93.4,50C93.4,74,74,93.4,50,93.4z"/> 
+
+    </g> 
+
+</g> 
+
+<line class="st0" x1="32.9" y1="29.8" x2="67.1" y2="29.8"/> 
+
+<line class="st0" x1="23" y1="46.7" x2="77" y2="46.7"/> 
+
+<line class="st0" x1="15.2" y1="63.6" x2="84.7" y2="63.6"/> 
+
+</SVG> 
