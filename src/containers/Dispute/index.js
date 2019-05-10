008ada8192ba3cd9de07270ed275481ae1eb8869
CM Ngan <plusonecm@gmail.com>
2019-05-10 15:19:41 +0800

    add coming soon icon

--- src/containers/Dispute/index.js
+++ src/containers/Dispute/index.js
@@ -1,41 +1,14 @@
 import React from 'react'
 import Text from '../../components/Text'
+import Image from '../../components/Image'
 import { Col } from '../../components/Layout'
-// import ScrollLoader from '../../components/ScrollLoader'
-
-// import {delay} from '../../lib/util/fetch';
-
-// const data = [
-//   1, 2, 3, 4, 5, 6,
-//   1, 2, 3, 4, 5, 6,
-//   1, 2, 3, 4, 5, 6,
-//   1, 2, 3, 4, 5, 6,
-//   1, 2, 3, 4, 5, 6,
-//   1, 2, 3, 4, 5, 6,
-//   1, 2, 3, 4, 5, 6,
-//   1, 2, 3, 4, 5, 6,
-//   1, 2, 3, 4, 5, 6,
-//   1, 2, 3, 4, 5, 6,
-//   1, 2, 3, 4, 5, 6,
-// ]
+import coming from '../../images/coming_soon.png'
 
 export default () => {
-  // const [array, setArray] = useState(data)
-  // const [loading, setLoading] = useState(false)
-  // const [hasNextPage, setHasNextPage] = useState(true)
-  // const id = 'TestId'
-  // async function loadMore() {
-  //   setLoading(true)
-  //   await delay(2000)
-  //   if(array.length > 50) setHasNextPage(false)
-  //   setArray([...array, ...data])
-  //   setLoading(false)
-  // }
   return (
-    <Col>
+    <Col center centerVertical height={'80vh'}>
+      <Image source={coming} size={'250px'} />
       <Text center VM note>Dispute function is under development, coming soon in later 2019!</Text>
-      {/* {array.map((a, i) => <Text key={i}>{a}</Text>)}
-      <ScrollLoader id={id} loadMore={loadMore} hasNextPage={hasNextPage} isLoading={loading} /> */}
     </Col>
   )
 }
--- src/containers/Market/Proposal.js
+++ src/containers/Market/Proposal.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import Text from '../../components/Text'
+import Image from '../../components/Image'
+import { Col } from '../../components/Layout'
+import coming from '../../images/coming_soon.png'
 
 export default function() {
-  return <Text center VM note>Proposal function is under development, coming soon in later 2019!</Text>
+  return (
+    <Col center centerVertical height={'80vh'}>
+      <Image source={coming} size={'250px'} />
+      <Text center VM note>Proposal function is under development, coming soon in later 2019!</Text>
+    </Col>
+  )  
 }
