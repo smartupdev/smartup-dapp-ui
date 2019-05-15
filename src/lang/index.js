7c71cfae4b35185dee4b49c4d820324c329f6680
unknown <karlis.chan@smartup.global>
2019-05-15 14:21:33 +0800

    langAddTcSc

--- src/lang/index.js
+++ src/lang/index.js
@@ -1,4 +1,4 @@
-export const currentLang = 'en' // TODO
+export const currentLang = 'tc' // TODO, i set as tc first
 
 function name(en, tc, sc) {
   return {
@@ -9,153 +9,153 @@ function name(en, tc, sc) {
 }
 
 export default {
-  result: name('RESULTS'),
-  search: name('Search'),
-  trade: name('Trade'),
+  result: name('RESULTS', '結果', '结果'),
+  search: name('Search', '搜尋', '搜寻'),
+  trade: name('Trade', '交易', '交易'),
   home: {
     tab: {
-      all: name('All'),
-      hot: name('Hottest'),
-      new: name('Newest'),
-      pop: name('Populous'),
-      rich: name('Richest'),
+      all: name('All', '全部'),
+      hot: name('Hottest', '最熱門', '最热门'),
+      new: name('Newest', '最新'),
+      pop: name('Populous', '最人氣', '最人气'),
+      rich: name('Richest', '最富有'),
     },
     table: {
-      name: name('NAME'),
-      change: name('24H CHANGE'),
-      price: name('SPOT PRICE'),
-      volume: name('24H VOLUME'),
-      cap: name('FUNDING POOL'),
-      graph: name('7D GRAPH')
+      name: name('NAME', '名稱', '名称'),
+      change: name('24H CHANGE', '24小時漲跌', '24小时涨跌'),
+      price: name('SPOT PRICE', '現貨價格', '现货价格'),
+      volume: name('24H VOLUME', '24小時成交量','24小时成交量'),
+      cap: name('FUNDING POOL', '總資金量', '总资金量'),
+      graph: name('7D GRAPH', '7天圖表', '7天图表')
     }
   },
   panel: {
     tab: {
-      portfolio: name('Portfolio'),
-      notification: name('Notifications'),
-      setting: name('Settings')
+      portfolio: name('Portfolio', '資料', '资料'),
+      notification: name('Notifications', '通知'),
+      setting: name('Settings', '設定', '设定')
     },
     portfolio: {
       wallet: {
-        title: name('Market Wallet'),
-        id: name('IDEA'),
-        ct: name('TOKENS'),
-        volume: name('CHANGE')
+        title: name('Market Wallet','巿場錢包','巿场钱包'),
+        id: name('IDEA', '巿場', '巿场'),
+        ct: name('TOKENS', '代幣'),
+        volume: name('CHANGE', '漲跌', '涨跌')
       },
       marketInfo: {
-        title: name('Global Market Information'),
-        totalSmartup: name('Total SMARTUP invested'),
-        totalMarket: name('Total number of markets'),
-        totalDiscussion: name('Total number of ongoing discussions')
+        title: name('Global Market Information', '全部巿場資訊', '全部巿场信息'),
+        totalSmartup: name('Total SMARTUP invested', '總SMARTUP投資額', '总SMARTUP投资额'),
+        totalMarket: name('Total number of markets', '全部巿場總數', '全部巿场总数'),
+        totalDiscussion: name('Total number of ongoing discussions', '正在討論的帖子數', '正在讨论的帖子数')
       },
       bookmark: {
-        title: name('Bookmarks')
+        title: name('Bookmarks', '書籤', '书签')
       }
     },
-    connectButton: name('Connect'),
-    term: name('Terms of Service'),
+    connectButton: name('Connect', '連接', '连接'),
+    term: name('Terms of Service', '服務條款', '服务条款'),
   },
   portfolio: {
-    wallet: name('Market Wallet'),
+    wallet: name('Market Wallet', '巿場錢包', '巿场钱包'),
     table: {
-      idea: name('IDEA'),
-      ct: name('TOKEN'),
-      volumn: name('VOLUME')
+      idea: name('IDEA', '巿場', '巿场'),
+      ct: name('TOKEN', '代幣', '代币'),
+      volumn: name('VOLUME', '漲跌', '涨跌')
     },
-    global: name('Global Market Information'),
-    sutInv: name('Total SMARTUP invested'),
-    mktNumber: name ('Total number of markets'),
-    disNumber: name('Total number of ongoing discussions'),
-    bookmark: name('Bookmarks')
+    global: name('Global Market Information', '全部巿場資訊', '全部巿场信息'),
+    sutInv: name('Total SMARTUP invested', '總SMARTUP投資額', '总SMARTUP投资额'),
+    mktNumber: name ('Total number of markets','全部巿場總數', '全部巿场总数'),
+    disNumber: name('Total number of ongoing discussions', '正在討論的帖子數', '正在讨论的帖子数'),
+    bookmark: name('Bookmarks', '書籤', '书签')
   },
   notification:{
-    day: name('d ago'),
-    hour: name('h ago')
+    day: name('d ago', '日前'),
+    hour: name('h ago', '小時前', '小时前')
   },
   setting: {
-    avatar: name('Avatar photo'),
-    dragFile: name('Drag file here'),
-    changeFile: name('Change file to upload'),
-    submit: name('Submit'),
-    cancel: name('Cancel'),
-    deactivate: name('Deactivate account')
+    avatar: name('Avatar photo', '個人圖片', '个人图片'),
+    dragFile: name('Drag file here', '拖放圖片至此', '拖放图片至此'),
+    changeFile: name('Change file to upload', '上載更改圖片', '上载更改图片'),
+    submit: name('Submit', '提交'),
+    cancel: name('Cancel', '取消'),
+    deactivate: name('Deactivate account', '停用帳戶', '停用帐户')
   },
   marketTab: {
-    trade: name('Trading'),
-    general: name('General'),
-    discussion: name('Discussions'),
-    proposal: name('Proposals'),
-    flag: name('Dispute'),
-    search: name('Search'),
+    trade: name('Trading', '交易'),
+    general: name('General', '一般'),
+    discussion: name('Discussions', '討論', '讨论'),
+    proposal: name('Proposals', '提案'),
+    flag: name('Dispute', '檢舉', '检举'),
+    search: name('Search', '搜尋', '搜寻'),
   },
   trading: {
-    period: name('7D'),
-    low: name('low'),
-    high: name('high'),
-    change: name('CHANGE (SINCE LAST 00:00)'),
-    price: name('SPOT PRICE'),
-    volume: name('VOLUME (SINCE LAST 00:00)'),
-    cap: name('FUNDING POOL'),
-    ct: name('NUMBER OF MARKET TOKEN MINTED'),
-    tradeTitle: name('Trade Token(s)'),
-    tradeText: name('You are performing token trading.'),
-    tradePay: name('PAY With'),
-    tradeReceive: name('RECEIVE'),
-    tradeButton: name('Trade'),
-    trans: name('Recent Transactions'),
+    period: name('7D', '7天'),
+    low: name('low', '低位'),
+    high: name('high', '高位'),
+    change: name('CHANGE (SINCE LAST 00:00)', '漲跌(自00:00)', '涨跌(自00:00)'),
+    price: name('SPOT PRICE', '現貨價格', '现货价格'),
+    volume: name('VOLUME (SINCE LAST 00:00)', '成交量(自00:00)'),
+    cap: name('FUNDING POOL', '總資金量', '总资金量'),
+    ct: name('NUMBER OF MARKET TOKEN MINTED', '巿場代幣已發行量', '巿场代币已发行量'),
+    tradeTitle: name('Trade Token(s)', '代幣交易', '代币交易'),
+    tradeText: name('You are performing token trading.', '你正在進行交易', '你正在进行交易'),
+    tradePay: name('PAY With', '付款'),
+    tradeReceive: name('RECEIVE' ,'收取'),
+    tradeButton: name('Trade', '交易'),
+    trans: name('Recent Transactions' ,'最新交易'),
     table: {
-      buySell: name('BUY/ SELL'),
-      user: name('USER'),
-      time: name('TIME'),
-      avgPrice: name('AVG PRICE IN SMARTUP'),
-      ct: name('TOKEN'),
-      buy: name('BUY'),
-      sell: name('SELL'),
-      stage: name('STATUS')
+      buySell: name('BUY/ SELL', '買入/賣出', '买入/卖出'),
+      user: name('USER', '用戶', '用户'),
+      time: name('TIME', '時間', '时间'),
+      avgPrice: name('AVG PRICE IN SMARTUP', 'SMARTUP平均價', 'SMARTUP平均价'),
+      ct: name('TOKEN', '代幣', '代币'),
+      buy: name('BUY', '買入', '买入'),
+      sell: name('SELL', '賣出', '卖出'),
+      stage: name('STATUS', '狀態', '状态')
     }
   },
   discussion: {
-    post: name('Posted by'),
-    day: name('d ago'),
-    hour: name('h ago'),
-    reply: name('reply'),
-    feedback: name('feedback'),
-    like: name('like'),
-    dislike: name('dislike'),
-    share: name('share'),
-    save: name('save'),
-    replies: name('more replies'),
-    vote: name('VOTE'),
+    post: name('Posted by', '發布自', '发布自'),
+    day: name('d ago', '日前'),
+    hour: name('h ago', '小時前', '小时前'),
+    reply: name('reply', '回覆', '回复'),
+    feedback: name('feedback', '回應', '回应'),
+    like: name('like', '讚好', '赞好'),
+    dislike: name('dislike', '不讚', '不赞'),
+    share: name('share', '分享'),
+    save: name('save', '存儲', '存储'),
+    replies: name('more replies', '更多回覆', '更多回复'),
+    vote: name('VOTE', '投票'),
   },
   general: {
-    info: name('Information'),
-    createTime: name('Create time'),
-    creator: name('Creator'),
-    overview: name('Overview'),
-    rule: name('Rules'),
-    sub: name('Subscribers'),
+    info: name('Information', '資訊', '信息'),
+    createTime: name('Create time', '創建時間', '创建时间'),
+    creator: name('Creator', '創建者', '创建者'),
+    overview: name('Overview', '概要'),
+    rule: name('Rules', '規則', '规则'),
+    sub: name('Subscribers', '訂戶', '订户'),
     table: {
-      name: name('NAME'),
-      rank: name('RANK'),
-      time: name('JOINED DATE'),
-      ct: name('TOKEN OWNED'),
+      name: name('NAME', '名稱', '名称'),
+      rank: name('RANK', '排名'),
+      time: name('JOINED DATE', '加入日期'),
+      ct: name('TOKEN OWNED', '持有代幣', '持有代币'),
     }
   },
   proposal: {
-    createTime: name('Create time'),
-    creator: name('Creator'),
-    remain: name('Remaining time'),
-    desp: name('description'),
-    option: name('Options'),
-    vote: name('VOTE'),
-    amt: name('Amount'),
-    approve: name('APPROVE'),
-    decline: name('DECLINE'),
-    para: name('Parameters'),
-    from: name('from'),
-    to: name('to'),
+    createTime: name('Create time', '創建時間', '创建时间'),
+    creator: name('Creator', '創建者', '创建者'),
+    remain: name('Remaining time', '剩餘時間', '剩余时间'),
+    desp: name('description', '描述'),
+    option: name('Options', '選項', '选项'),
+    vote: name('VOTE', '投票'),
+    amt: name('Amount', '數量', '数量'),
+    approve: name('APPROVE', '通過', '通过'),
+    decline: name('DECLINE', '否決'),
+    para: name('Parameters', '參數', '参数'),
+    from: name('from', '由'),
+    to: name('to', '至'),
   },
   faq: {
-    title: name('FAQ'),
+    title: name('FAQ', '常見問題', '常见问题'),
   }
 }
