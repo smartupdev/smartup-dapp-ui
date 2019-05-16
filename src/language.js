5e30d87b128c589a19ee0e15725686f4a3b329ec
CM Ngan <plusonecm@gmail.com>
2019-05-16 14:41:43 +0800

    set default lang as browser settings

--- src/language.js
+++ src/language.js
@@ -2,8 +2,14 @@ import React, {createContext, useContext, useState} from 'react'
 import { initialLang, languages } from './lang'
 
 const LANG_STORAGE_KEY = 'lang'
+const tcList = ['zh-TW', 'zh-HK', 'zh']
+const scList = ['zh-CN', 'zh-SG']
 function getInitLang() {
-  return window.localStorage.getItem(LANG_STORAGE_KEY) || initialLang
+  const browserLang = navigator.language
+  return window.localStorage.getItem(LANG_STORAGE_KEY) || 
+  tcList.includes(browserLang) ? 'tc' : 
+  scList.includes(browserLang) ? 'sc' :
+  'en'
 }
 export const LangContext = createContext()
 
