e69e55bc5ed5df00104136d97901956a6bb206ea
CM Ngan <plusonecm@gmail.com>
2019-05-21 16:29:18 +0800

    remove no used code

--- src/store.js
+++ src/store.js
@@ -1,16 +1,7 @@
 import { applyMiddleware, createStore } from 'redux';
 import reducer from './reducer';
 // import { createLogger } from 'redux-logger'
-import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+// import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import thunk from 'redux-thunk';
 
-const getMiddleware = () => {
-  return process.env.NODE_ENV === 'production' ?
-    applyMiddleware(thunk)
-    :
-    composeWithDevTools(applyMiddleware(thunk
-      // , createLogger()
-      ))
-};
-
-export const store = createStore(reducer, getMiddleware());
+export const store = createStore(reducer, applyMiddleware(thunk));
