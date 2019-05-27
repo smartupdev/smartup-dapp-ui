21f9894b0773a0499bd952c6e30ded41de9ea882
CM Ngan <plusonecm@gmail.com>
2019-05-27 12:21:38 +0800

    update use guide txt

--- src/containers/userGuide/index.js
+++ src/containers/userGuide/index.js
@@ -11,14 +11,14 @@ import { Row, Col } from '../../components/Layout'
 
 const TextBody = ({children}) => <TextWithLink newline VXS note>{children}</TextWithLink>
 
-const installChrome = `a.	到https://www.google.com/chrome/
+const installChrome = `a.	到 https://www.google.com/chrome/ 
 b.	点撃”Download Chrome”
 c.	下载Chrome后点撃ChromeSetup.exe安装
 `
 
 const installMetamask = `MetaMask是第三方网桥，让您能在Google Chrome浏览器中的访问分布式网络和dapp，如 SmartUp 孵化器平台。 它允许您直接在浏览器中运行Ethereum dApp，而无须运行完整的Ethereum节点。 
 您可能会将其视为存储ETH和SmartUp的ETH钱包。 只需安装MetaMask扩展，点击与SmartUp 孵化器平台连接，即可开始充分利用SmartUp孵化器平台。 
-a.	使用Chrome到https://metamask.io/
+a.	使用Chrome到 https://metamask.io/ 
 b.	点撃”GET CHROME EXTENSION”
 c.	点撃”加到 Chrome”
 d.	完成安装后点撃”Get Started”
@@ -38,10 +38,51 @@ c.	在测试水管中获取测试用的ETH
 d.	点击”request 1 ether from faucet”
 `
 
-const addSut = `a.	到https://faucet.smartup.global/static/index.html
+const addSut = `a.	到 https://faucet.smartup.global/static/index.html 
 b.	在MetaMask复制钱包的地址并把地址贴上钱包地址一栏
 c.	按"Give me 100W SUT" 按钮
 d.	等待1-2分钟，币会自动添加到你的钱包裹
+`
+const loginTxt = `a.	进入 https://ipfs.smartup.global/ipfs/QmaNVSpC47Br7MPu2iqZ9HYWXNetq3wqG1SAE2JpTmKt6K/#/ 
+b.	点击Connect链接登入MetaMask
+c.	在弹出的MetaMask上点击”Sign”完成登入
+`
+const personalCenterTxt = `a.	于平台右面是Portfolio页面，可以检视钱包、已投资巿场、书韱、通知及个人设定
+b.	点击”Setting”
+c.	上载个人图片，点击”Submit”完成上载，图片可多次更改
+d.	更改名称（每个帐户只可命名一次）输入名字后，点击”Submit”，确认无误后点击” Confirm”
+`
+const notificationTxt = `现时平台有三种通知，日后开放更多功能后或有更多通知类型。 
+1.	创建巿场：创建巿场后，无论成功或失败均会收到通知
+2.	买入通知：在买入巿场代币后，无论成功或失败均会收到通知
+3.	卖出通知：在卖出巿场代币后，无论成功或失败均会收到通知
+`
+const createMktTxt = `a.	在平台左边导览栏，点击第二个分页创建巿场
+b.	输入巿场名称及巿场简介
+c.	加入巿场图像及封面图片
+d.	点击”Next”预览代币价格曲线
+e.	存入巿场按金（现时固定为2500个SmartUp Token）
+f.	在MetaMask确认交易，等待1-2分钟完成创建巿场
+`
+
+const tradeTxt = `a.	在主页中找到想交易的巿场，点击展开查阅，再点击”Trade”进入巿场交易页面
+b.	点选BUY或SELL
+c.	输入交易的巿场代币数量
+d.	勾选右面”Agree To Terms of Service”
+e.	点击”Trade”
+f.	在MetaMask确认交易
+`
+
+const mktDiscussionTxt = `发起讨论：
+a.	进入巿场后，点撃上方分页列中的”Discussions”
+b.	点击“+” 发布新帖子
+c.	输入帖子标题及内容，可选择上载一张图片
+d.	点击”Submit”成功发布帖子
+
+回复帖子：
+a.	在”Discussions”点击进入帖子
+b.	在帖子的最下方输入回复内容
+c.	点击”Reply”成功回复
 `
 
 const sections = [
@@ -52,14 +93,14 @@ const sections = [
     { title: '增值SmartUp', body: addSut },
   ]},
   { title: '登入及个人设定', sections: [
-    { title: '登入', body: addSut },
-    { title: '个人设定', body: addSut },
-    { title: '通知', body: addSut },
+    { title: '登入', body: loginTxt },
+    { title: '个人设定', body: personalCenterTxt },
+    { title: '通知', body: notificationTxt },
   ]},
   { title: '使用巿场', sections: [
-    { title: '创建巿场', body: addSut },
-    { title: '巿场交易', body: addSut },
-    { title: '巿场讨论', body: addSut },
+    { title: '创建巿场', body: createMktTxt },
+    { title: '巿场交易', body: tradeTxt },
+    { title: '巿场讨论', body: mktDiscussionTxt },
   ] },
 ]
 
