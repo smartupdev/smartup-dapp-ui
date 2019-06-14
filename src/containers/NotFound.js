59840cedf1aea44f97a5f208656e2034f2c679cb
CM Ngan <plusonecm@gmail.com>
2019-06-14 13:41:40 +0800

    styling page not found page

--- src/containers/NotFound.js
+++ src/containers/NotFound.js
@@ -1,16 +1,15 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import Image from '../components/Image'
 import Text from '../components/Text'
 import { Col } from '../components/Layout'
 import PageNotFoundImage from '../images/404.png'
 function NotFound() {
   return (
-    <Col center centerVertical flex={1}>
+    <Col center centerVertical HS flex={1}>
       <Image source={PageNotFoundImage} size={'300px'} />
-      <Text XL note VS bold>OH MY GOSH! YOU FOUND IT!!!</Text>
-      <Text note>Looks like the page you're trying to visit doesn't exists.</Text>
-      <Text note>Please check the URL and try your luck again.</Text>
+      <Text center XL note VS bold>OH MY GOSH! YOU FOUND IT!!!</Text>
+      <Text center note>Looks like the page you're trying to visit doesn't exists.</Text>
+      <Text center note>Please check the URL and try your luck again.</Text>
     </Col>
   )
 }
