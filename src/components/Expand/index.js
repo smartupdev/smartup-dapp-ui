3fc30ced7186ee5360f3cffa009d5c45e06e6465
CM Ngan <plusonecm@gmail.com>
2019-06-18 16:41:29 +0800

    fix expand height issue

--- src/components/Expand/index.js
+++ src/components/Expand/index.js
@@ -1,15 +1,24 @@
 import React from 'react'
-import styled, { css } from 'styled-components'
+import styled, { css, keyframes } from 'styled-components'
 import { Col } from '../Layout'
 
+const collapseAnimation = keyframes`
+  0%   { max-height: 100vh; }
+  100% { max-height: 0; }
+`
+
+const expandAnimation = keyframes`
+  0%   { max-height: 0; }
+  99% { max-height: 100vh; }
+  100% { max-height: fit-content; }
+`
+
 export const Expanded = styled(Col)`
-  max-height: 0;
-  transition: max-height 0.15s ease-out;
+
+  animation: ${collapseAnimation} .5s cubic-bezier(.04,.85,.22,.97) forwards;
   overflow: hidden;
   ${p => p.isExpanded && css`
-    max-height: ${p.maxHeight};
-    transition: max-height 0.25s ease-in;
-    overflow: auto;
+    animation: ${expandAnimation} .5s ease-in forwards;
   `}
 `
 
