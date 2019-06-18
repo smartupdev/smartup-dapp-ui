c2aae8cb32d69e02bcdb546e15879d59799c04cb
CM Ngan <plusonecm@gmail.com>
2019-06-18 14:28:31 +0800

    fix lang initial value

--- src/language.js
+++ src/language.js
@@ -10,14 +10,19 @@ export const languageOptions = [
 const LANG_STORAGE_KEY = 'lang'
 const tcList = ['zh-TW', 'zh-HK', 'zh']
 const scList = ['zh-CN', 'zh-SG']
+function setLangStore(langText) {
+  window.localStorage.setItem(LANG_STORAGE_KEY, langText)
+  return langText
+}
 export function getLang() {
   const browserLang = navigator.language
-  return window.localStorage.getItem(LANG_STORAGE_KEY) || 
-  (
+  const storedLang = window.localStorage.getItem(LANG_STORAGE_KEY)
+  if(storedLang) return storedLang
+  return setLangStore((
     tcList.includes(browserLang) ? 'tc' : 
     scList.includes(browserLang) ? 'sc' :
     'en'
-  )
+  ))
 }
 export const LangContext = createContext()
 
@@ -25,7 +30,7 @@ export const LangProvider = ({children}) => {
   const [lang, setLang] = useState(getLang())
   function changeLang(langText) {
     setLang(langText)
-    window.localStorage.setItem(LANG_STORAGE_KEY, langText)
+    setLangStore(langText)
   }
   return (
     <LangContext.Provider value={[languages[lang], lang, changeLang]}>
