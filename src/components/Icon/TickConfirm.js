17f52668afa284060e4e98d5488bc2363651746d
unknown <karlis.chan@smartup.global>
2019-09-04 18:02:01 +0800

    add TickConfirm

---
+++ src/components/Icon/TickConfirm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SVG } from './index'
+export default ({ color, ...rest }) => 
+<SVG {...rest}  color={color} version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+  viewBox="0 0 512 512" xmlSpace="preserve">
+<g>
+ <g>
+  <path d="M504.5,75.5c-10-10-26.2-10-36.2,0L161.6,382.2L43.7,264.3c-10-10-26.2-10-36.2,0c-10,10-10,26.2,0,36.2l136,136
+   c10,10,26.2,10,36.2,0l324.8-324.8C514.5,101.7,514.5,85.5,504.5,75.5z"/>
+ </g>
+</g>
+</SVG>
